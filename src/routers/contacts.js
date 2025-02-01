378ca9a2392a64d75d56f401b99facae4d12b03b
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-01 20:25:53 +0100

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { deleteContact } from "../controllers/contacts.js";
+import * as contactsController from "../controllers/contacts.js";
 
-const router = express.Router();
+const router = express.Router(); // Оголошуємо router перед використанням
+
+// Отримати всі контакти
+router.get("/", contactsController.getAllContacts);
 
 // DELETE-Route для видалення контакту
-router.delete("/:contactId", deleteContact);
+router.delete("/:contactId", contactsController.deleteContact);
 
 export default router;
+
 
 
