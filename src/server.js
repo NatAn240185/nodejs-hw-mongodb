563663c3f25d1ef0a0fe1b4e37b9be98adbc515f
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-03 14:59:36 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,48 @@
-import express from "express";
-import contactsRouter from "./routers/contacts.js";
-import { errorHandler } from "./middlewares/errorHandler.js";
-import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import cors from 'cors';
+import express from 'express';
+import 'dotenv/config';
+import pino from 'pino-http';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import routes from './routers/contacts.js';
 
 const app = express();
 
-app.use(express.json());
-app.use("/contacts", contactsRouter);
-app.use(notFoundHandler);
+app.use(cors());
+
+app.use(routes);
+
+app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+
+app.use('*', notFoundHandler);
+
 app.use(errorHandler);
 
+
+
 export function setupServer() {
-  return app;
+        try {
+            const PORT = process.env.PORT || 3000;
+            app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+
 
 
