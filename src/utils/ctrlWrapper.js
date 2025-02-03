ef643b8cc5e4f3c44a219e36be89a57f1174e9f4
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-03 14:58:34 +0100

    Update ctrlWrapper.js

--- src/utils/ctrlWrapper.js
+++ src/utils/ctrlWrapper.js
@@ -1,8 +1,10 @@
-export const ctrlWrapper = (ctrl) => async (req, res, next) => {
+export function ctrlWrapper(controller) {
+  return async (req, res, next) => {
     try {
-      await ctrl(req, res, next);
-    } catch (err) {
-      next(err);
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
     }
   };
+}
   
