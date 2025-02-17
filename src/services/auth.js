8b9e91800d6e2d74febd6047d61008800a9a4217
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-17 17:33:06 +0100

    add all

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,7 +4,6 @@ import bcrypt from "bcrypt";
 import { sessionMon } from "../models/session.js";
 import crypto from "node:crypto";
 
-
 export async function registerUser(payload) {
     const user = await userMon.findOne({ email: payload.email });
     
