c07772cbd5a542d30a22b4be49fdbd3d00c92990
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-26 16:52:22 +0100

    Update parsePaginationParams.js

--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -1,8 +1,25 @@
-export const parsePaginationParams = (query) => {
-    const page = parseInt(query.page, 10) || 1;
-    const limit = parseInt(query.limit, 10) || 10;
-    const skip = (page - 1) * limit;
-    
-    return { page, limit, skip };
-  };
-  
+function parseNumber(value, defaultValue) {
+    if (typeof value !== "string") {
+        return defaultValue;
+    }
+
+    const parsedValue = parseInt(value); 
+
+    if (Number.isNaN(parsedValue) === true) {
+        return defaultValue;
+    }
+
+    return parsedValue;
+}
+
+export function parsePaginationParams(query) {
+    const { page, perPage } = query;
+
+    const parsedPage = parseNumber(page, 1);
+    const parsedPerPage = parseNumber(perPage, 10);
+
+    return {
+        page: parsedPage,
+        perPage: parsedPerPage,
+    }; 
+}
