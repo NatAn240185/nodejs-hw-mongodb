929949299a91c132c1d5917b33534a719fe25221
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-26 16:45:26 +0100

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,19 +1,11 @@
-import { HttpError } from 'http-errors';
+import { isHttpError } from 'http-errors';
 
-export const errorHandler = (err, req, res, next) => {
-  // Перевірка, чи отримали ми помилку від createHttpError
-  if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: err,
-    });
-    return;
+export function errorHandler(error, req, res, next) {
+  if (isHttpError(error) === true) {
+    return res
+      .status(error.statusCode)
+      .send({ status: error.statusCode, message: error.message });
   }
 
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
-    data: err.message,
-  });
-};
+  res.status(500).send({ status: 500, message: 'Something went wrong' });
+}
