228957c01647a1a45453459f88b9289cee7f16fb
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-26 16:49:20 +0100

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,7 +4,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { contactSchemaJoi, replaceContactSchemaJoi } from "../validation/contactValidation.js";
-import { upload } from '../middlewares/upload.js';
+
 
 const router = express.Router();
 const jsonParser = express.json();
@@ -13,11 +13,11 @@ router.get('/contacts', ctrlWrapper(getContactsControllers));
 
 router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactsIdControllers));
 
-router.post('/contacts', upload.single('photo'), jsonParser, validateBody(contactSchemaJoi), ctrlWrapper(createContactController));
+router.post('/contacts', jsonParser, validateBody(contactSchemaJoi), ctrlWrapper(createContactController));
 
 router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
-router.patch('/contacts/:contactId', upload.single('photo'), isValidId, jsonParser, validateBody(replaceContactSchemaJoi), ctrlWrapper(updateContactController));
+router.patch('/contacts/:contactId', isValidId, jsonParser, validateBody(replaceContactSchemaJoi), ctrlWrapper(updateContactController));
 
 
 
