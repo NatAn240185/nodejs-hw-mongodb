9ee3a7a6f4f8d834317ed48a2337d7f13410f25a
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-26 15:06:16 +0100

    Update saveFileToCloudinary.js.js

--- src/utils/saveFileToCloudinary.js.js
+++ src/utils/saveFileToCloudinary.js.js
@@ -6,7 +6,7 @@ import { getEnvVar } from './getEnvVar.js';
 
 cloudinary.v2.config({
   secure: true,
-  cloud_name: getEnvVar("CLOUDINARY_CLOUD_NAME"),
+  cloud_name: getEnvVar("CLOUDINARY_API_NAME"),
   api_key: getEnvVar("CLOUDINARY_API_KEY"),
   api_secret: getEnvVar("CLOUDINARY_API_SECRET"),
 });
