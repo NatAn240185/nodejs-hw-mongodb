4c3bdd421262f41b97f444f1b2bf7006aded4629
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2025-02-26 07:35:21 +0100

    add all

--- src/utils/saveFileToCloudinary.js.js
+++ src/utils/saveFileToCloudinary.js.js
@@ -3,11 +3,10 @@ import cloudinary from 'cloudinary';
 import fs from 'node:fs/promises';
 
 import { getEnvVar } from './getEnvVar.js';
-import { CLOUDINARY } from '../constants/index.js';
 
 cloudinary.v2.config({
   secure: true,
-  cloud_name: getEnvVar("CLOUD_NAME"),
+  cloud_name: getEnvVar("CLOUDINARY_CLOUD_NAME"),
   api_key: getEnvVar("CLOUDINARY_API_KEY"),
   api_secret: getEnvVar("CLOUDINARY_API_SECRET"),
 });
